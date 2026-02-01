0711253f48bb9604b0c761fbc8cda6e554c3f19a
Aayush Chauhan <at290608@gmail.com>
2026-02-01 23:21:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Agence from './pages/Agence'
+import Projects from './pages/Projects'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-zinc-900 text-white'>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/agence' element={<Agence/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
